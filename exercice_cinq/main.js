67ed798166ee5866de5c27091be4f78747df9116
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-02-13 16:01:44 +0100

    modif

--- exercice_cinq/main.js
+++
@@ -1,7 +0,0 @@
-function affichage() {
-
-                alert(Math.trunc(document.getElementById("premier_nombre").value) 
-                *document.getElementById("deuxieme_nombre").value);
-
-
-}
