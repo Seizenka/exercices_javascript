f41650204c692937a3adef961785c42ae95892ab
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-02-14 10:14:54 +0100

    ajout

---
+++ base 1/exercice_huit/js/main.js
@@ -0,0 +1,21 @@
+function question() {
+
+    if(document.getElementById("age").value >= 18) 
+        {
+           alert('Vous êtes majeur');
+        }
+
+    else {
+        alert('Vous êtes mineur');
+    }
+        
+}
+
+/*let age = prompt('Quel âge avez-vous ? (en années)');
+let majeur = (age >= 18);
+
+if(majeur)
+{
+   alert('Vous êtes majeur');
+   // instructions dans le bloc a placer ici
+}*/
