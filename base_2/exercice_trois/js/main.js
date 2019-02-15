b379597f8fac56a3f5872c3700416050faf88c84
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-02-15 14:06:48 +0100

    ajout-exercice-trois

---
+++ base_2/exercice_trois/js/main.js
@@ -0,0 +1,15 @@
+
+
+function affiche() {
+
+    let a = 3;
+    let b = 2;
+
+    function triple(x) { return x * 3; }
+
+    alert(triple (a));
+    console.log("résultat de la première valeur");
+
+    alert(triple (b));
+    console.log("résultat de la deuxième valeur");
+}
