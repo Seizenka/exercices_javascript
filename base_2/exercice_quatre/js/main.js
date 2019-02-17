68ad4a10174efe84f1bcb84559b72b764b1d0172
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-02-17 17:18:46 +0100

    ajout

---
+++ base_2/exercice_quatre/js/main.js
@@ -0,0 +1,14 @@
+function affiche() {
+
+    let tab = [-2, 1, 4];
+
+    function additionne(x) {
+        return x + 2;
+    }
+    alert(additionne(tab[tab.length * 0]));
+    alert(additionne(tab[tab.length - 1]));
+
+
+     
+}
+
