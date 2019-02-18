fd47f59954ecf649e5279c1e2bcd7687d51fe7f6
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-02-18 16:26:12 +0100

    modif

--- base_4/exercice_un/js/main.js
+++ base_4/exercice_un/js/main.js
@@ -3,3 +3,10 @@ function bordure(){
 
     document.getElementById("image1").style.border= "3px outset red";
 }
+
+document.getElementById("image1").addEventListener("mouseout", sansBordure);
+function sansBordure(){
+
+    document.getElementById("image1").style.border= "";
+
+}
