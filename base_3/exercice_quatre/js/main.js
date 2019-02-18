5fa294d738c565565c6ea759f3899dd612cba846
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-02-18 11:43:23 +0100

    ajout-exercice-quatre

---
+++ base_3/exercice_quatre/js/main.js
@@ -0,0 +1,15 @@
+function formulaire() {
+
+    let nom = document.getElementById("nom");
+    let prenom = document.getElementById("prenom");
+    let ville = document.getElementById("ville");
+    console.log(nom);
+    console.log(prenom);
+    console.log(ville);
+    
+    nom.setAttribute("value", " ");
+    prenom.setAttribute("value", " ");
+    ville.setAttribute("value", " ");
+   }
+ 
+     
