b8b4dc39f7184e4d5c2421d84e57e40e7b1c0c79
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-02-27 11:59:08 +0100

    ajout-canvas-exercices

---
+++ canvas/boat/js/main.js
@@ -0,0 +1,26 @@
+function draw() {
+    let c = document.getElementById("mon_canvas");
+    let ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(150,80);
+    ctx.lineTo(300,230);
+    ctx.lineTo(150,300);
+    ctx.closePath();
+    ctx.fillStyle = "lightblue";
+    ctx.fill();
+    //fin de la voile
+
+    //coque du bâteau
+    ctx.beginPath();
+    ctx.moveTo(50,250);
+    ctx.lineTo(100,300);
+    ctx.lineTo(250,300);
+    ctx.lineTo(300,250);
+    ctx.fillStyle = "peru";
+    ctx.strokeStyle = "sienna"; // Définition de la couleur de contour
+    ctx.lineWidth = 5;         // Définition de la largeur de ligne
+    ctx.fill();            // Application du remplissage
+    ctx.stroke();          // Application du contour
+
+
+}
